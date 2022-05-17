7f05286ac612ccd12be759af9d5438e1f0f652fc
pyropiyush <sahupiyush91@gmail.com>
2022-05-18 01:12:12 +0530

    commit 2

--- experiMent.java
+++ experiMent.java
@@ -11,16 +11,29 @@ public class experiMent{
             arr[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j <= arr.length; j++) {
-                for (int k = i; k < j; k++) {
-
-                    System.out.print(arr[k] + " ");
-                }
-                System.out.println();
-            }
-
-        }
+        int data=sc.nextInt();
+        int lo=0;
+        int hi=arr.length-1;
+        int ceil=0;
+        int floor=0;
+while(lo<=hi){
+    int mid=(lo+hi)/2;
+    if (arr[mid] > data){
+        hi=mid-1;
+        ceil= arr[mid];
     }
+    else if(data > arr[mid]){
+        lo=mid+1;
+        floor=arr[mid];
+    }
+    else{
+        ceil=arr[mid];
+        floor=arr[mid];
+        break;
+    }
+}
 
+        System.out.println(ceil);
+        System.out.println(floor);
+    }
 }
